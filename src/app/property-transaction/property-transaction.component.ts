644e7b3dc6c52debe8899f5477cd6a1eb2561e84
Sun Hainan <hainan.sun@gmail.com>
2019-08-11 20:54:41 +0800

    Update charts for the trans price

--- src/app/property-transaction/property-transaction.component.ts
+++ src/app/property-transaction/property-transaction.component.ts
@@ -3,6 +3,7 @@ import { PTransaction } from '../model/ptransaction';
 import { SchoolListService }  from '../school-service/school-list.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-property-transaction',
@@ -22,16 +23,48 @@ export class PropertyTransactionComponent implements OnInit {
     this.getTransactions();
   }
 
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+  public barChartLabels = [];
+  public barChartType = 'line';
+  public barChartLegend = true;
+  public barChartData = [];
+
   getTransactions(): void {
     const pname = this.route.snapshot.paramMap.get('pname');
     this.schoolListService.getPropertyTransactions(pname)
       .subscribe(trans => {
         this.transactions = trans;
       });
-
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  priceHistoryChart(): void {
+    this.barChartData = [];
+    this.barChartLabels = [];
+
+    var priceData = [];
+    var psfData = [];
+    var pname = this.transactions[0].pname;
+    for(var i = this.transactions.length-1; i >= 0; i=i-1) {
+      this.barChartLabels.push(moment(this.transactions[i].date).format('YYYY-MM'));
+   //   priceData.push(this.transactions[i].price);
+      psfData.push(this.transactions[i].psf);
+    }
+    //this.barChartData.push({data: priceData, label: "price"});
+    this.barChartData.push({data: psfData, label: pname + " (psf)"});
+    
   }
 }
