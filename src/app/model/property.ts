e9c725b9e31eeecd3ac350b19b21417c28d88510
Sun Hainan <hainan.sun@gmail.com>
2019-08-26 22:53:18 +0800

    Update the UI logic for grc properties

--- src/app/model/property.ts
+++ src/app/model/property.ts
@@ -30,5 +30,4 @@ export class GrcProperty {
   blk: string;
   postal: string;
   grc: string;
-  distance: number;
 }
--- src/app/property-grc/property-grc.component.ts
+++ src/app/property-grc/property-grc.component.ts
@@ -3,6 +3,8 @@ import { GrcProperty } from '../model/property';
 import { SchoolListService }  from '../school-service/school-list.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { IStringHash } from '../model/ihash';
+
 
 @Component({
   selector: 'app-property-grc',
@@ -13,6 +15,8 @@ export class PropertyGrcComponent implements OnInit {
 
   grcProperties: GrcProperty[];
   school: string;
+  projectHash : IStringHash = {};
+  distanceAsc: boolean;
 
   constructor(private route: ActivatedRoute,
     private schoolListService: SchoolListService,
@@ -20,6 +24,7 @@ export class PropertyGrcComponent implements OnInit {
 
   ngOnInit() {
     this.getProperties();
+    this.distanceAsc = false;
   }
 
   getProperties(): void {
@@ -28,6 +33,14 @@ export class PropertyGrcComponent implements OnInit {
     this.schoolListService.getPropertyByGrc(grc)
       .subscribe(properties => {
         this.grcProperties = properties;
+
+        this.grcProperties.forEach(e => {
+          this.projectHash[this.school+e.project+e.blk] = ">2km";
+          this.schoolListService.getNearbyPropertyBySchoolByBlk(this.school,e.project,e.blk)
+            .subscribe(p => {
+              this.projectHash[this.school+e.project+e.blk] = p[0].distance.toFixed(3).toString() + "km";
+            })
+        })
       })
   }
 
@@ -35,4 +48,24 @@ export class PropertyGrcComponent implements OnInit {
     this.location.back();
   }
 
+  orderByDistance(): void {
+    if (this.distanceAsc) {
+      this.grcProperties.sort((n1: GrcProperty, n2: GrcProperty) => {
+        if(this.projectHash[this.school+n1.project+n1.blk] < this.projectHash[this.school+n2.project+n2.blk]) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+    } else {
+      this.grcProperties.sort((n1: GrcProperty, n2: GrcProperty) => {
+        if(this.projectHash[this.school+n1.project+n1.blk] > this.projectHash[this.school+n2.project+n2.blk]) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+    }
+    this.distanceAsc = !this.distanceAsc;
+  }
 }
